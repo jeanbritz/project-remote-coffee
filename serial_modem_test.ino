e14db224d71aaf19c9b8d64300fbb0f6d6c47b5d
Jean Britz <britz.jean@gmail.com>
2014-05-03 09:50:20 +0200

    Updated the test with sending AT+CREG?

--- serial_modem_test.ino
+++ serial_modem_test.ino
@@ -1,86 +1,112 @@
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 
-
-#define unoTXPin (13) // Uno -> GSM
-#define unoRXPin (12) // Uno <- GSM
-#define unoBaudRate (115200)
-
-#define gsmBaudRate (9600)
-
-SoftwareSerial gsmSerial (unoRXPin, unoTXPin);
+#define MEGA_BAUD_RATE (9600)
+#define GSM_BAUD_RATE (9600)
+#define HORIZONTAL_LINE ("================================")
+#define AT_COMMAND_TIMEOUT (2000)
 
 char temp;
+int8_t totalTests = 2;
+int8_t countTests = 0;
 int8_t answer = 0;
 
 void setup()
 {
 
-  Serial.begin(unoBaudRate);
-  gsmSerial.begin(gsmBaudRate);
-  answer = sendATcommand("AT", "OK", 2000);
-  if(answer == 0)
-	{
-		Serial.print("Failure");
-	}
-	else
-	{
-		Serial.print("Success");	
-	
-	}
-}
+  Serial.begin(MEGA_BAUD_RATE);
+  Serial.println(HORIZONTAL_LINE);
+  Serial.println(F("        GSM Modem Test          "));
+  Serial.println(HORIZONTAL_LINE);
+  Serial.println(HORIZONTAL_LINE);
+  Serial.println(F("| Type \'t\' to start the test   |"));
+  Serial.println(HORIZONTAL_LINE);
+  Serial1.begin(GSM_BAUD_RATE);
 
+}
 void loop()
 {
-	
-	while(gsmSerial.available() > 0)
-	{
-		
-			temp = (char)gsmSerial.read();
-			Serial.print(">> ");Serial.print(temp);Serial.println();
-			
-		
-		
-	
-	}
-		
-	
+
+  if(Serial.read() == 't')
+  {
+
+    while(countTests < totalTests)
+    {
+      countTests = 0;
+      Serial.print(F("\n*****    Test: "));
+      Serial.print(F("Send \'AT\'"));
+      Serial.println(F("    *****"));
+      if(sendATcommand("AT", "OK", 2000))
+      {
+        Serial.println(F("Response: OK"));
+        countTests++;
+      }
+      else
+      {
+        Serial.println(F("Response: GSM not responding"));
+        Serial.println(F("Error: Test failed!"));
+      }
+      Serial.print(F("\n*****    Test: "));
+      Serial.print(F("Send \'AT+CREG?\'"));
+      Serial.println(F("    *****"));
+      if(sendATcommand("AT+CREG?", "+CREG: 0,1", 2000))
+      {
+        Serial.println("Response: +CREG: 0,1");
+        Serial.println("Info: Registered on the home network"); 
+        countTests++;
+      }
+      while(Serial1.available() > 0)
+      {
+        temp = Serial1.read();
+        Serial.write(temp);
+      }
+
+      delay(1000);
+    }
+    Serial.println(HORIZONTAL_LINE);
+    Serial.println("|     Test has ended    |");
+    Serial.println(HORIZONTAL_LINE);
+  }
+
 }
-int8_t sendATcommand(char* ATcommand, char* expected_answer1,
+int8_t sendATcommand(char* ATcommand, char* expected_answer,
 unsigned int timeout)
 {
 
-	uint8_t x=0,  answer=0;
-	char response[100];
-	unsigned long previous;
+  uint8_t x=0,  answer=0;
+  char response[100];
+  unsigned long previous;
 
-	memset(response, '\0', 100);    // Initialize the string
+  memset(response, '\0', 100);    // Initialize the string
 
-	delay(100);
+  delay(100);
 
-	while( gsmSerial.available() > 0) gsmSerial.read();    // Clean the input buffer
+  while( Serial1.available() > 0) Serial1.read();    // Clean the input buffer
 
-	gsmSerial.print(ATcommand);    // Send the AT command
-	gsmSerial.write(13);
+  Serial1.print(ATcommand);    // Send the AT command
+  Serial1.write(13);           // Send carriage return char
 
-	x = 0;
-	previous = millis();
+  x = 0;
+  previous = millis();
 
-	// this loop waits for the answer
-	do{
+  // this loop waits for the answer
+  do{
 
-		if(gsmSerial.available() != 0){
-			response[x] = gsmSerial.read();
-			x++;
-			// check if the desired answer is in the response of the module
-			if (strstr(response, expected_answer1) != NULL)
-			{
-				answer = 1;
-			}
-		}
-		// Waits for the answer with time out
-	}
-	while((answer == 0) && ((millis() - previous) < timeout));
+    if(Serial1.available() != 0){
+      response[x] = Serial1.read();
+      x++;
+      // check if the desired answer is in the response of the module
+      if (strstr(response, expected_answer) != NULL)
+      {
+        answer = 1;
+      }
+    }
+   // Serial.println(response);
+    // Waits for the answer with time out
+  }
+  while((answer == 0) && ((millis() - previous) < timeout));
 
-	return answer;
+  return answer;
 }
+
+
+
